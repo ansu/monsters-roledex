3030de076ccca52e11460344ce13d36f01caaf29
Kuliza-447 <kuliza-447@Kuliza-447.local>
2020-08-14 20:14:37 +0530

    adding files for github pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import CardList from './components/card-list/card-list.container'
+import SearchField from './components/search-field/search-field.component'
+class App extends React.Component {
+   constructor(props){
+     super(props);
+     this.state =  {
+       monsters:[],
+       searchText:'',
+       
+     }
+   }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   textUpdated = (e) => {
+    console.log("coming")
+     this.setState({searchText:e.target.value})
+   }
+
+   componentDidMount(){
+     fetch("https://jsonplaceholder.typicode.com/users")
+     .then(response => response.json())
+     .then(users => this.setState({monsters: users,filterMonsters:users}))
+     .catch(error => console.log(error))
+   }
+
+  render(){
+    const { monsters, searchText} = this.state;
+    const filterMonsters = monsters.filter(monster => monster.name.includes(searchText));
+    return(
+          <div className="App">
+            <h1> Monsters Roldex</h1>
+            <SearchField placeHolder='Search' handleChange={this.textUpdated}/>
+              <CardList monster = {filterMonsters}> </CardList>    
+          </div>
+        )
+  }
 }
 
 export default App;
