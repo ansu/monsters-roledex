4f32f60487347ecad2dd2df0898bc9bb4a6f7550
Kuliza-447 <kuliza-447@Kuliza-447.local>
2020-08-14 20:21:06 +0530

    Updates

---
+++ precache-manifest.ff1923fcf1421c8caee85e9518d9644e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "922c81eb64d6942f2b2d1052c98ad481",
+    "url": "/mosters-roledex/index.html"
+  },
+  {
+    "revision": "79875001ce6f6c0384a1",
+    "url": "/mosters-roledex/static/css/main.bf1e8cb6.chunk.css"
+  },
+  {
+    "revision": "9dadd1a5607abc36395e",
+    "url": "/mosters-roledex/static/js/2.db3844ca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mosters-roledex/static/js/2.db3844ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79875001ce6f6c0384a1",
+    "url": "/mosters-roledex/static/js/main.a14bbfc1.chunk.js"
+  },
+  {
+    "revision": "c6f8ab22618c23a59cdd",
+    "url": "/mosters-roledex/static/js/runtime-main.7df31cca.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mosters-roledex/static/media/logo.5d5d9eef.svg"
+  }
+]);
